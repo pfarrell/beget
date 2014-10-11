0561e5e7cdd59370ae92a581dc362aace1e35ce3
Pat Farrell <mr.patfarrell@gmail.com>
2014-10-11 11:47:51 -0700

    sequel added

--- app/models.rb
+++ app/models.rb
@@ -1,1 +1,14 @@
-require 'models/my_model'
+require 'sequel'
+require 'redis'
+require 'logger'
+  
+$console = Logger.new STDOUT
+DB = Sequel.connect(
+  "mysql2://#{ENV["BEMUSED_DB_USER"]}:#{ENV["BEMUSED_DB_PASS"]}@#{ENV["BEMUSED_DB_HOST"]}/#{ENV["BEMUSED_DB_NAME"]}",
+  logger: $console)
+DB.sql_log_level = :debug
+DB.extension(:pagination)
+
+Sequel::Model.plugin :timestamps
+Sequel::Model.plugin :json_serializer
+
