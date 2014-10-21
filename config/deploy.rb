bc24f153fb91e84fa8ef2c3bb965b85894d9d31f
Patrick Farrell <mr.patfarrell@gmail.com>
2014-10-21 08:06:31 -0700

    moved gumbo to public

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_via, :remote_cache
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
- set :linked_dirs, %w{log tmp gumbo}
+ set :linked_dirs, %w{log tmp public/gumbo}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
